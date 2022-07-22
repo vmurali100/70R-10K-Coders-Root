cba79d82331b05ba120277b2b1882c1c19beecaf
Chokkallasrikanth <srikanthchokkalla@gmail.com>
2022-07-22 23:31:55 +0530

    CRUD added

---
+++ JAVASCRIPT/JS7_CRUD/Create.js
@@ -0,0 +1,19 @@
+var users = [
+    {
+        id: 1,
+        email: "srikanthchokkalla@gmail.com",
+        username: "srikanthCh",
+        password: "12345"
+    },
+];
+function addUser() {
+    var user = {
+        id: document.getElementById("id").value,
+        email: document.getElementById("email").value,
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value,
+    }
+    users.push(user);
+    displayUsers()
+    clearTable()
+}
