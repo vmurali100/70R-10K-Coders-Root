036f8cb2c8752f93ccef809b64a7ec530541fa2c
Meetmsmanasa <106164924+Meetmsmanasa@users.noreply.github.com>
2022-07-23 06:03:40 +0530

    push

---
+++ Manasa.JS/JS7_CRUD/update.js
@@ -0,0 +1,40 @@
+var globalIndex =null 
+
+function edituser(index){
+
+    globalIndex = index 
+   
+    document.getElementById("id").value=users[index] .id
+    document.getElementById("email").value=users[index].email
+    document.getElementById("username").value=users[index] .username
+    document.getElementById("password").value=users[index] .password
+    swapbuttons(true)
+}
+
+function updateuser(){
+
+    var user = {
+    
+        id: document.getElementById("id").value,
+        email: document.getElementById("email").value,
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value,
+    };
+
+    displayuser();
+    clearform();
+    swapbuttons(false);
+
+}
+
+function swapbuttons(value){
+    if (value == true){
+        document.getElementById("updateBtn").style.display="block";
+        document.getElementById("addBtn").style.display="none";
+     }
+     else {
+        document.getElementById("updateBtn").style.display="none";
+        document.getElementById("addBtn").style.display="block";
+     }
+
+    }
