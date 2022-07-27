2730746df196eea7a127f19c0eeba11ff596e059
keerthiprasanna7 <keerthiprasannajntuh@gmail.com>
2022-07-27 18:04:01 +0530

    push

---
+++ Javascript/JS7_CRUD_API/create.js
@@ -0,0 +1,35 @@
+function createUser() {
+    var user = {
+        id: document.getElementById("id").value,
+        email: document.getElementById("email").value,
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value,
+
+    };
+    // users[globalIndex] = user;
+    // displayUsers();
+    // clearTable()
+}
+
+
+
+
+var api_url = "http://localhost:3000/users"
+
+
+var createUser = new XMLHttpRequest();
+createUser.onreadystatechange = function() {
+    if (createUser.readyState == 4 && deleteuser.status == 200) {
+
+        console.log("User Deleted")
+        getDataFromServer()
+    }
+    createUser.open("POST", api_url + users[i].id, user);
+    createUser.setRequestHeader("Content-Type", "application/json")
+    createUser.send(JSON.stringify(user))
+
+
+
+
+    createUser.send()
+}
