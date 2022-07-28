4e4faacdf11a14ac9381d95c6c56c217fe63b542
AMILPURAJAY <amilpurajaykumar333@gmail.com>
2022-07-28 23:59:30 +0530

    crud_api

--- JAVASCRIPT(JS)/day6/create.js
+++ JAVASCRIPT(JS)/day6/create.js
@@ -1,11 +1,13 @@
 function validate (){
     var idvalue = document.getElementById("id").value;
-    console.log(idvalue)
     var textpattern=/[a-z]/i
-     
+    var emailvalue=document.getElementById("email").value;
+    var emailpattern= /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/i 
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
 // check for id n alphabets
 
-    if(idvalue>0 &&  !textpattern.test(idvalue)){
+    if(idvalue.length>0 &&  !textpattern.test(idvalue) && emailpattern.test(emailvalue) && username.length>2&&password.length>6){
 document.getElementById("addBtn").removeAttribute("disabled")
     }
     else{
@@ -20,4 +22,24 @@ document.getElementById("addBtn").removeAttribute("disabled")
     {
         document.querySelector("p").style.display="none" 
     }
+    if (!emailpattern.test(emailvalue) && emailvalue.length>0) {
+        document.querySelector("p1").style.display="block"
+    }else
+    {
+        document.querySelector("p1").style.display="none" 
+    }
+    if (username.length<6) {
+        document.querySelector("p2").style.display="block" 
+    }else
+    {
+        document.querySelector("p2").style.display="none" 
+    }
+    if (password.length<6) {
+        document.querySelector("p3").style.display="block" 
+    }else
+    {
+        document.querySelector("p3").style.display="none" 
+    }
 } 
+
+  
---
+++ JAVASCRIPT(JS)/day7/crud_api/update.js
@@ -0,0 +1,56 @@
+
+function edituser(index) {
+    globalindex = index;
+    var newuser = ajay[index]
+    document.getElementById("fname").value = newuser.fname
+    document.getElementById("lname").value = newuser.lname
+    document.getElementById("tel").value = newuser.tel
+    document.getElementById("address").value = newuser.address
+    document.getElementById("city").value = newuser.city
+    document.getElementById("state").value = newuser.state
+    document.getElementById("zip").value = newuser.zip
+   
+   
+
+    swapbuttons(true)
+}
+
+var globalindex = null;
+
+function swapbuttons(value) {
+    console.log(value);
+    if (value == true) {
+        document.getElementById("updateBtn").style.display = "block"
+        document.getElementById("addBtn").style.display = "none"
+    } else {
+        document.getElementById("updateBtn").style.display = "none"
+        document.getElementById("addBtn").style.display = "block"
+    }
+}
+function updateuser() {
+    var users = {
+        fname: document.getElementById("fname").value,
+        lname: document.getElementById("lname").value,
+        tel: document.getElementById("tel").value,
+        address: document.getElementById("address").value,
+        city: document.getElementById("city").value,
+        state: document.getElementById("state").value,
+        zip: document.getElementById("zip").value,
+    }
+    ajay[globalindex] = users
+    // displayusers();
+    // cleartable();
+    // swapbuttons(false)
+
+    var apiurl="http://localhost:3000/users"
+    var updateuser=new XMLHttpRequest();
+    updateuser.onreadystatechange=function(){
+        if (updateuser.readyState ==4 && updateuser.status==200) { 
+          getdatafromserver()
+}
+    }
+    updateuser.open("PUT",apiurl+users[globalindex].id);
+    updateuser.setRequestHeader("content-type","application/json")
+    updateuser.send(JSON.stringify(users))
+}
+
