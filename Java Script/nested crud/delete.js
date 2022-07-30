22a5a91b522ffc94f4467dcc104675e6d85db2a9
vishnuvardhanyella <vardhanyella@gmail.com>
2022-07-30 16:16:05 +0530

    ADD

--- Java Script/nested crud/delete.js
+++ Java Script/nested crud/delete.js
@@ -1,1 +1,5 @@
-function delet
+function deleteuser(){
+    var deleteUrl = api_url+selecteduser.id
+    console.log(deleteUrl)
+    fetch(deleteUrl,{method:"DELETE"}).then(()=>{getnestedusers()})
+}
