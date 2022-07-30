66541fc0abea447b23dc09ae04bd802e169e2053
Meetmsmanasa <106164924+Meetmsmanasa@users.noreply.github.com>
2022-07-30 15:52:58 +0530

    push ajax with details

---
+++ Manasa.JS/Practice/CRUD_API_AJAX/sample6_ajax_details/create.js
@@ -0,0 +1,32 @@
+
+
+var details = []
+
+function addUser() {
+    
+    var detail = {
+        fname: document.getElementById("fname").value,
+        lname: document.getElementById("lname").value,
+        tel: document.getElementById("tel").value,
+        address: document.getElementById("address").value,
+        city: document.getElementById("city").value,
+        state: document.getElementById("state").value,
+        zip: document.getElementById("zip").value,
+    };
+    
+    // displayUsers()
+    // clearTable()
+    // swapButton(false)
+    var api_url="http://localhost:3000/details/"
+    var addUser = new XMLHttpRequest();
+    addUser.onreadystatechange = function () {
+        if (addUser.readyState == 4 && addUser.status == 200){
+            console.log("user Deleted")
+            getDataFromServer()
+        }
+    }
+    addUser.open("POST",api_url);
+    addUser.setRequestHeader("Content-Type","application/json")
+
+    addUser.send(JSON.stringify(detail))
+}
