3f32f706b0b712e65e6678f71ea84209ec3c7141
vishnuvardhanyella <vardhanyella@gmail.com>
2022-08-08 18:03:13 +0530

    ADD CHANGES

---
+++ my/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./COMPONENTS/Header";
+import Cards from "./COMPONENTS/Cards";
+import Footer from "./COMPONENTS/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Cards />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ my/src/COMPONENTS/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+
+function Header() {
+  return (
+    <div>
+        <PersonIcon/>
+        <ForumIcon/>
+    </div>
+  )
+}
+
+export default Header
