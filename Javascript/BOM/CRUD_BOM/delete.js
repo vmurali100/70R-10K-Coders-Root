051417f90568786322718b768f3e3310177a8457
keerthiprasanna7 <keerthiprasannajntuh@gmail.com>
2022-08-10 20:12:57 +0530

    push

---
+++ Javascript/BOM/CRUD_BOM/delete.js
@@ -0,0 +1,13 @@
+function deleteUser(i) {
+    var api_url = "http://localhost:3000/users/"
+
+    var deleteUser = new XMLHttpRequest();
+    deleteUser.onreadystatechange = function() {
+        if (deleteUser.readyState == 4 && deleteUser.status == 200) {
+            console.log("User Deleted")
+            getDataFromServer()
+        }
+    }
+    deleteUser.open("DELETE", api_url + users[i].id);
+    deleteUser.send()
+}
