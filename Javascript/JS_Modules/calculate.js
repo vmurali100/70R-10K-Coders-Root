c6ba1e5f9520ec62c46c2b9621186182b6a3d71a
keerthiprasanna7 <keerthiprasannajntuh@gmail.com>
2022-08-15 17:20:45 +0530

    push

---
+++ Javascript/JS_Modules/calculate.js
@@ -0,0 +1,12 @@
+ export default function multiplyNumbers(num1, num2) {
+     return num1 * num2;
+ }
+
+ export function addNumbers(num1, num2) {
+     return num1 + num2;
+
+ }
+
+ export function subtractNumbers(num1, num2) {
+     return num1 - num2;
+ }
---
+++ Javascript/JS_Modules/student.js
@@ -0,0 +1,7 @@
+class Student {
+    constructor(fname, lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+}
+export var student1 = new Student("vamsi", "krishna")
