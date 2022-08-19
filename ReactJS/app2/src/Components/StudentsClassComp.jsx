6227a17a1c86a2dbe373f1129c6f1d7ad198da71
Meetmsmanasa <106164924+Meetmsmanasa@users.noreply.github.com>
2022-08-19 17:53:10 +0530

    push app2

--- ReactJS/app2/src/Components/StudentsClassComp.jsx
+++ ReactJS/app2/src/Components/StudentsClassComp.jsx
@@ -4,13 +4,26 @@ export class StudentsClassComp extends Component{
     constructor(){
         super()
         this.state={
-            users:["Ram","Ravi","Sam","Sundar"]
+            // users:["Ram","Ravi","Sam","Sundar"]
+            person:{
+                "fname": "Shannon",
+                "lname": "Nicholson",
+                "tel": "(291)289-3849",
+                "address": "5502 Odio Rd",
+                "city": "Mesa",
+                "state": "VA",
+                "zip": 57118
+            }
         }
     }
     render(){
         return <div>
-            {this.state.users.map((val,i)=>{
+            {/* {this.state.users.map((val,i)=>{
                 return <p key={i}>{val}</p>
+            })} */}
+            {Object.keys(this.state.person).map((val,i)=>{
+                // return <p key={i}>{val}</p>
+                return <p key={i}>{val}:-{this.state.person[val]}</p>
             })}
         </div>
     }
