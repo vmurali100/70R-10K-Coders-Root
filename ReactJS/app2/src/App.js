3029378235d12921451d8ac821b6fc8a714819d6
Chokkallasrikanth <srikanthchokkalla@gmail.com>
2022-08-21 18:28:03 +0530

    app2 added

---
+++ ReactJS/app2/src/App.js
@@ -0,0 +1,13 @@
+import Number from "./Components/1";
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Number/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/app2/src/Components/1.jsx
@@ -0,0 +1,8 @@
+import { Srikanth } from "./main";
+
+export default function Number(){
+    return <div>
+         {/* <h2>Hello ReactJS My Mobile Number is 8790xxxxxx</h2> */}
+    <Srikanth/>
+    </div>
+}
