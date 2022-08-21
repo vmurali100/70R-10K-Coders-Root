eaed15ac24c2294d32b9a7639f8e09ab7f4b99e8
keerthiprasanna7 <keerthiprasannajntuh@gmail.com>
2022-08-21 21:09:33 +0530

    push

--- ReactJS/app3/src/Components/Prasanna.jsx
+++ ReactJS/app3/src/Components/Prasanna.jsx
@@ -1,3 +1,9 @@
-export default function Prasanna(){
-    return <h2>Hello from Prasanna Component</h2>
+import Prasanna from "./PrasannaClassComp";
+
+export  default function Keerthi(){// one  component can return one html
+    return<div> 
+        <h2>Hello from Keerthi Component</h2>
+        
+        <Prasanna/>
+    </div>
 }
---
+++ ReactJS/app4/src/Components/Sample.jsx
@@ -0,0 +1,21 @@
+import Sample from "./SampleClassComp";
+
+
+export  default function Keerthi(){
+    return<div> 
+        <h2>Hello from Keerthi Component</h2>
+
+        
+        <Sample/>
+    </div>
+}
+
+// import Prasanna from "./PrasannaClassComp";
+
+// export  default function Keerthi(){// one  component can return one html
+//     return<div> 
+//         <h2>Hello from Keerthi Component</h2>
+        
+//         <Prasanna/>
+//     </div>
+// }
