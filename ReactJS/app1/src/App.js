2109d00565858cc3e06e541eff790a42d2bb6594
Chokkallasrikanth <srikanthchokkalla@gmail.com>
2022-08-22 12:07:05 +0530

    1

---
+++ ReactJS/app1/src/App.js
@@ -0,0 +1,12 @@
+import { Srikanth } from './Components/Srikanth';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <Srikanth/>
+    </div>
+  );
+}
+
+export default App;
--- ReactJS/app1/src/Components/Srikanth.jsx
+++ ReactJS/app1/src/Components/Srikanth.jsx
@@ -1,10 +1,12 @@
 import Chokkalla from "./Chokkalla";
 import { SrikanthClass } from "./SrikanthClassComp";
+import StudentClassComp from "./StudentClassComp";
+
 
 export function Srikanth(){
     return <div>
         <h2>Hello From ReactJs Intelligence</h2>
-        {/* <Chokkalla/>
-        <SrikanthClass/> */}
+        <SrikanthClass/>
+        <StudentClassComp/>
     </div>
 }
