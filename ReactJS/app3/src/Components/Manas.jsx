37957aeea627529c78728c2cdbefbd24f5857fae
Meetmsmanasa <106164924+Meetmsmanasa@users.noreply.github.com>
2022-08-22 20:45:39 +0530

    fun

--- ReactJS/app3/src/Components/Manas.jsx
+++ ReactJS/app3/src/Components/Manas.jsx
@@ -1,10 +1,12 @@
 import { Content } from "./ContentClassComp";
 import { DetailsClassComp } from "./DetailsClassComp";
 import { ManasClass } from "./ManasClassComp";
+import Sample from "./Sample";
 
 export function Manas() {
     return <div>
         <h2>Hello from Manas</h2>
+        {/* <Sample/> */}
        {/* <DetailsClassComp/> */}
        {/* <ManasClass/> */}
        <Content/>
