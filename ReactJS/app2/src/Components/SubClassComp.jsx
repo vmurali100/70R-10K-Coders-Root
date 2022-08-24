e97b8b1353c66ad53c3efc23d7d7e1084ae9e25f
Meetmsmanasa <106164924+Meetmsmanasa@users.noreply.github.com>
2022-08-24 07:10:24 +0530

    fun

---
+++ ReactJS/app2/src/Components/SubClassComp.jsx
@@ -0,0 +1,38 @@
+import { Component } from "react";
+
+export class SubClassComp extends Component{
+    constructor(props){
+        super()
+        console.log(props)
+    }
+
+    
+
+    render(){
+        return <div>
+            <h2>Hello from Child Component</h2>
+            <button onClick={this.props.changeMessage}>Change Message</button>
+
+            <p> Message From Parent Component : {this.props.msg}</p> 
+
+            {/* //it means we can pass one particular section here */}
+
+            {Object.keys(this.props.userDetails).map((val,i)=>{
+                return <p key={i}>{this.props.userDetails[val]}</p>
+            })}
+
+            <ul>
+            {this.props.allStudents.map((student,i)=>{
+                return <li key={i}>{student}</li>
+            })}
+            </ul>
+
+
+            {/* we cannot display the object direct, we have to convert it too Array using object.keys */}
+            <hr />
+            {/* we can send the data from one component to other component*/}
+            {/* If we are sending data from parent component to child component then in child component it is called as props */}
+            <h4>This Child component will get displayed using parent component and props is similar as state which is used in child component and props cannot be modified</h4>
+        </div>
+    }
+}
--- ReactJS/app2/src/index.js
+++ ReactJS/app2/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-import { Manasa } from './Components/Manasa';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Manasa />
+  <App/>
 );
 
 // If you want to start measuring performance in your app, pass a function
