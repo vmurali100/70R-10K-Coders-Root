c7bee882bb1b4e27467067a2d6147622a1715542
keerthiprasanna7 <keerthiprasannajntuh@gmail.com>
2022-08-24 16:08:11 +0530

    push

--- ReactJS/app1/src/Components/Product.jsx
+++ ReactJS/app1/src/Components/Product.jsx
@@ -1,12 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 import ProductoneClass from "./ProductoneClassComp";
-import ProductClass from "./ProductsClassComp";
+//import ProductClass from "./ProductsClassComp";
 
 export  default function Keerthi(){// one  component can return one html
     return<div> 
         <h2>Hello from Keerthi Component</h2>
         
         {/* <ProductClass/> */}
-        {/* <ProductoneClass/> */}
+         {/* <ProductoneClass/>  */}
         
     </div>
 }
