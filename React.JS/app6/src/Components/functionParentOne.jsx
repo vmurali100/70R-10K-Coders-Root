e8e1b88738b1afcda3c4b47e07ffba362b15d22b
PrudhviDeve <rathna.prudhvi.6180@gmail.com>
2022-08-25 10:16:17 +0530

    app6 fun and class components

---
+++ React.JS/app6/src/Components/functionParentOne.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { ChildMessage } from "./functionChildOne";
+
+export function Message() {
+    const [welcomeMsg, setWelcomeMsg] = useState("Hello World");
+    const [person, setPerson] = useState({ fname: "Prudhvi", lname: "Rathna" });
+    const [users, setUsers] = useState(["Sam", "Chaithu"]);
+
+    const changeMessage = () => {
+        setWelcomeMsg("This is from parent functional component")
+    }
+
+    const changePerson = () => {
+        setPerson({ fname: "Vinay", lname: "Kumar" })
+    }
+
+    const changeUsers = () => {
+        setUsers(["KCR", "KTR"])
+    }
+
+    return <div>
+        <h2>Hello from function parent class</h2>
+        <hr />
+        <ChildMessage
+            welcomeMsg={welcomeMsg}
+            person={person}
+            users={users}
+            changeMessage={changeMessage}
+            changePerson={changePerson}
+
+            changeUsers={changeUsers}
+        />
+    </div>
+
+}
