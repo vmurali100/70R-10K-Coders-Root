73094b6e326edf5ebd971422a07af3ed36289838
keerthiprasanna7 <keerthiprasannajntuh@gmail.com>
2022-08-31 06:57:40 +0530

    push forms

---
+++ ReactJS/app7/src/App.jsx
@@ -0,0 +1,14 @@
+import { User } from './Components/User';
+//import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <User/>
+      
+    </div>
+  );
+}
+
+export default App;
