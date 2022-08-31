951f6f36fb7b935fbb7a6eb49e2489a4c1151b99
Meetmsmanasa <106164924+Meetmsmanasa@users.noreply.github.com>
2022-08-31 14:04:57 +0530

    pushing class component

---
+++ ReactJS/app7/src/Components/User.jsx
@@ -0,0 +1,35 @@
+import React,{useState} from 'react';
+
+export const User = () => {
+    //const [firstName, setfirstName] = useState("")//to declare variable,to pass additional funct. need to place arrow functn
+    const [person, setperson] = useState({firstName:"",lastName:"",email:""})
+
+     const handleChange=(e)=>{//synthetic element
+        console.log(e.target.value)//to read element
+        var inputname =e.target.name
+        var newperson ={...person}//creating an copy of object
+        newperson[inputname]=e.target.value
+        setperson(newperson)
+        console.log(inputname)
+
+        //setfirstName(e.target.value)//to update,used for inputfield 1
+     }
+        const handleSubmit=()=>{
+            console.log(person)
+        }
+
+     
+  return (
+    <div>
+       <form>
+           <label htmlFor='firstName'>First Name:</label>
+           <input type="text" name="firstName" value={person.firstName} onChange={(e)=>{handleChange(e)}} /> <br/>
+           <label htmlFor="lastName">Last Name:</label>
+           <input type="text" name="lastName"value={person.lastName} onChange={(e)=>{handleChange(e)}}/> <br/>
+           <label htmlFor="email">Email:</label>
+           <input type="text" name="email"value={person.email} onChange={(e)=>{handleChange(e)}}/> <br/>
+           <button onClick={handleSubmit} type="button">Add User</button>
+       </form>
+    </div>
+  )
+}
---
+++ ReactJS/app7/src/Components/UserClass.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+export default class UserClass extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      person:{firstName:"",lastName:"",email:""}
+    }
+  }
+   handleChange=(e)=>{//synthetic element
+    console.log(e.target.value)//to read element
+    var inputname =e.target.name
+    var newperson ={...this.state.person}//creating an copy of object
+    newperson[inputname]=e.target.value
+    this.setState({person:newperson})
+    //setperson(newperson)
+    // console.log(inputname)
+
+    //setfirstName(e.target.value)//to update,used for inputfield 1
+ }
+     handleSubmit=()=>{
+        console.log(this.state.person)
+    }
+  render() {
+    return (
+      <div>
+         <form>
+              <label htmlFor='firstName'>First Name:</label>
+              <input type="text" name="firstName" value={this.state.person.firstName} onChange={(e)=>{this.handleChange(e)}} /> <br/>
+              <label htmlFor="lastName">Last Name:</label>
+              <input type="text" name="lastName"value={this.state.person.lastName} onChange={(e)=>{this.handleChange(e)}}/> <br/>
+              <label htmlFor="email">Email:</label>
+              <input type="text" name="email"value={this.state.person.email} onChange={(e)=>{this.handleChange(e)}}/> <br/>
+             <button onClick={this.handleSubmit} type="button">Add User</button>
+       </form>
+      </div>
+    );
+  }
+}
