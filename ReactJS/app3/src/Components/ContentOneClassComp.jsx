82823bac7bf5fd2ad11767f25235c510f4eae9a3
Meetmsmanasa <106164924+Meetmsmanasa@users.noreply.github.com>
2022-08-31 13:02:54 +0530

    push

---
+++ ReactJS/app3/src/Components/ContentOneClassComp.jsx
@@ -0,0 +1,60 @@
+import { Component } from "react";
+
+export class ContentOne extends Component {
+    constructor() {
+        super()
+        this.state = {
+            sentence: "",
+            list: {},
+            users: []
+        }
+
+    }
+    changeSentence = () => {
+        this.setState({
+            sentence: "Hello from Event Handler app3"
+        })
+
+    }
+
+    changeList = () => {
+        this.setState({ list: { fname: "Manas", lname: "Raj", email: "raj@gmail.com", age : 25, state: "AndhraPradesh" } })
+    }
+
+    displayUsers = () => {
+        this.setState({ users: ["Tel", "Hin", "Eng", "Soc"] })
+    }
+
+    handleDelete = (i) => {
+        console.log(i)
+        let allUsers = [...this.state.users]
+        allUsers.splice(i,1)
+        this.setState({users: allUsers})
+    }
+
+    render() {
+        return <div>
+            <button onClick={this.changeSentence}>Set Message</button>
+            <h2>Hello from Sentence Class Componenet</h2>
+            <p>Content Message is : {this.state.sentence}</p>
+
+            <hr />
+
+            <button onClick={this.changeList}> Change Person</button>
+            <p>First Name: {this.state.list.fname}</p>
+            <p>Last Name: {this.state.list.lname}</p>
+            <p>Email Address: {this.state.list.email}</p>
+            <p>Age: {this.state.list.age}</p>
+            <p>State: {this.state.list.state}</p>
+
+            <hr />
+
+            <h2>Users</h2>
+            <button onClick={this.displayUsers}>Display Users</button>
+            {this.state.users.map((user, i) => {
+                return <p onClick={() => { this.handleDelete(i) }} key={i}>{user}</p>
+            })}
+        </div>
+    }
+
+}
