15b2baf41430e66b3084fc8430cee3dc1c9cb21f
keerthiprasanna7 <keerthiprasannajntuh@gmail.com>
2022-08-31 07:17:23 +0530

    push forms

--- ReactJS/app7/src/App.jsx
+++ ReactJS/app7/src/App.jsx
@@ -1,11 +1,13 @@
 import { User } from './Components/User';
+import UserClass from './Components/UserClass';
 //import logo from './logo.svg';
 //import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <User/>
+      {/* <User/> */}
+      <UserClass/>
       
     </div>
   );
--- ReactJS/app7/src/Components/User.jsx
+++ ReactJS/app7/src/Components/User.jsx
@@ -2,14 +2,15 @@ import React,{useState} from 'react';
 
 export const User = () => {
     //const [firstName, setfirstName] = useState("")//to declare variable,to pass additional funct. need to place arrow functn
-    const [person, setperson] = useState({firstName:"",lastName:""})
+    const [person, setperson] = useState({firstName:"",lastName:"",email:""})
 
      const handleChange=(e)=>{//synthetic element
         console.log(e.target.value)//to read element
         var inputname =e.target.name
         var newperson ={...person}//creating an copy of object
         newperson[inputname]=e.target.value
-        setperson(newperson)
+        this.setState({person:newperson})
+        //setperson(newperson)
         console.log(inputname)
 
         //setfirstName(e.target.value)//to update,used for inputfield 1
@@ -26,6 +27,8 @@ export const User = () => {
            <input type="text" name="firstName" value={person.firstName} onChange={(e)=>{handleChange(e)}} /> <br/>
            <label htmlFor="lastName">Last Name:</label>
            <input type="text" name="lastName"value={person.lastName} onChange={(e)=>{handleChange(e)}}/> <br/>
+           <label htmlFor="email">Email:</label>
+           <input type="text" name="email"value={person.email} onChange={(e)=>{handleChange(e)}}/> <br/>
            <button onClick={handleSubmit} type="button">Add User</button>
        </form>
     </div>
