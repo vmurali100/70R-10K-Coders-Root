925d7f9fc8e6a40d0ea9874e6500417d6caedb81
Meetmsmanasa <106164924+Meetmsmanasa@users.noreply.github.com>
2022-09-01 06:43:11 +0530

    push functional and class components

---
+++ ReactJS/FORMS/app8/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { Sample } from './Components/Sample';
+import SampleClass from './Components/SampleClass';
+import { SampleOne } from './Components/SampleOne';
+import SampleOneClass from './Components/SampleOneClass';
+import { SampleThird } from './Components/SampleThird';
+import SampleThirdClass from './Components/SampleThirdClass';
+import { Sampletwo } from './Components/Sampletwo';
+import SampleTwoClass from './Components/SampleTwoClass';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Sample/> */}
+      {/* <SampleClass/> */}
+      {/* <SampleOne/> */}
+      {/* <SampleOneClass/> */}
+      {/* <Sampletwo/> */}
+      {/* <SampleTwoClass/> */}
+      {/* <SampleThird/> */}
+      <SampleThirdClass/>
+
+
+    </div>
+  );
+}
+
+export default App;
