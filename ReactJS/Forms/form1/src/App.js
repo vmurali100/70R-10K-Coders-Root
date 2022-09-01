4e4df1756bd103e2df06ef7f4608e62678998855
Chokkallasrikanth <srikanthchokkalla@gmail.com>
2022-09-01 10:36:15 +0530

    Form5 with Class&Function Components are Added

--- ReactJS/Forms/form1/src/App.js
+++ ReactJS/Forms/form1/src/App.js
@@ -1,7 +1,9 @@
 /* eslint-disable react/jsx-pascal-case */
 // import logo from './logo.svg';
 import './App.css';
-import { Function_Form4 } from './Forms/Function_Form4';
+import { Function_Form5 } from './Forms/Function_Form5';
+// import Class_Form5 from './Forms/Class_Form5';
+// import { Function_Form4 } from './Forms/Function_Form4';
 // import Class_Form4 from './Forms/Class_Form4';
 // import { Function_Form3 } from './Forms/Function_Form3';
 // import Class_Form3 from './Forms/Class_Form3';
@@ -17,10 +19,12 @@ function App() {
       {/* <Class_Form2/> */}
       {/* <Class_Form3/> */}
       {/* <Class_Form4/> */}
+      {/* <Class_Form5/> */}
       {/* <Function_Form1/> */}
       {/* <Function_Form2/>  */}
       {/* <Function_Form3/> */}
-      <Function_Form4/>
+      {/* <Function_Form4/> */}
+      <Function_Form5/>
     </div>
   );
 }
