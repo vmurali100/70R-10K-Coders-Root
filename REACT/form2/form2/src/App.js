8e50fca216b665fc38f2fcfa4a6151d535e7bf2d
vishnuvardhanyella <vardhanyella@gmail.com>
2022-09-03 14:12:42 +0530

    add

--- REACT/form2/form2/src/App.js
+++ REACT/form2/form2/src/App.js
@@ -4,14 +4,16 @@ import { Form2crud } from './Components/Form2crud';
 import { Form3crud } from './Components/Form3crud';
 import { Form4crud } from './Components/Form4crud';
 import { Form5crud } from './Components/Form5crud';
+import { Form1crud } from './Components/Form1crud';
 
 function App() {
   return (
     <div className="App">
+      {/* <Form1crud/> */}
       {/* <Form2crud/> */}
       {/* <Form3crud/> */}
       {/* <Form4crud/> */}
-      <Form5crud/>
+      {/* <Form5crud/> */}
     </div>
   );
 }
