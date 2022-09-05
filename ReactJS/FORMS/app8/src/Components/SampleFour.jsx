ee09496affd051614d74d2a030370c702b87731c
Meetmsmanasa <106164924+Meetmsmanasa@users.noreply.github.com>
2022-09-05 03:00:55 -0700

    push crud apps

--- ReactJS/FORMS/app8/src/Components/SampleFour.jsx
+++ ReactJS/FORMS/app8/src/Components/SampleFour.jsx
@@ -1,18 +1,18 @@
 import React,{useState} from 'react';
 
 export const SampleFour = () => {
-    const [teddy, setteddy] = useState({username:"",password:"",emailaddress:"",dateofbirth:"",height:"",weight:""})
+    const [user, setuser] = useState({username:"",password:"",emailaddress:"",dateofbirth:"",height:"",weight:""})
 
      const handleChange=(e)=>{//synthetic element
         console.log(e.target.value)//to read element
         var inputname =e.target.name
-        var newteddy ={...teddy}//creating an copy of object
-        newteddy[inputname]=e.target.value
-        setteddy(newteddy)
+        var newuser ={...user}//creating an copy of object
+        newuser[inputname]=e.target.value
+        setuser(newuser)
 
      }
      const handleSubmit=()=>{
-        console.log(teddy)
+        console.log(user)
     }
          
   return (
@@ -21,17 +21,17 @@ export const SampleFour = () => {
        <form>
            
            <label htmlFor="username">Username :</label>
-           <input type="text" name="username"value={teddy.username} onChange={(e)=>{handleChange(e)}}/> <br/>
+           <input type="text" name="username"value={user.username} onChange={(e)=>{handleChange(e)}}/> <br/>
            <label htmlFor="password">Password :</label>
-           <input type="text" name="password"value={teddy.password} onChange={(e)=>{handleChange(e)}}/> <br/>
+           <input type="text" name="password"value={user.password} onChange={(e)=>{handleChange(e)}}/> <br/>
            <label htmlFor="emailaddress">Email Address :</label>
-           <input type="text" name="email"value={teddy.emailaddress} onChange={(e)=>{handleChange(e)}}/> <br/>
+           <input type="text" name="email"value={user.emailaddress} onChange={(e)=>{handleChange(e)}}/> <br/>
            <label htmlFor="dateofbirth">Date of Birth:</label> 
-            <input type="text" name="dateofbirth" value={teddy.dateofbirth} onChange={(e)=>{handleChange(e)}} /><br/>
+            <input type="text" name="dateofbirth" value={user.dateofbirth} onChange={(e)=>{handleChange(e)}} /><br/>
             <label htmlFor="height">Height :</label> 
-            <input type="text" name="height" value={teddy.height} onChange={(e)=>{handleChange(e)}} /><br/>
+            <input type="text" name="height" value={user.height} onChange={(e)=>{handleChange(e)}} /><br/>
             <label htmlFor="weight">Weight:</label> 
-            <input type="text" name="weight" value={teddy.weight} onChange={(e)=>{handleChange(e)}} /><br/>
+            <input type="text" name="weight" value={user.weight} onChange={(e)=>{handleChange(e)}} /><br/>
            
            <button onClick={handleSubmit} type="button">SUBMIT</button>
 
--- ReactJS/FORMS/app8/src/Components/SampleFourClass.jsx
+++ ReactJS/FORMS/app8/src/Components/SampleFourClass.jsx
@@ -5,21 +5,21 @@ export default class SampleFourClass extends Component {
       super(props)
     
       this.state = {
-        teddy:{username:"",password:"",emailaddress:"",dateofbirth:"",height:"",weight:""}
+        user:{username:"",password:"",emailaddress:"",dateofbirth:"",height:"",weight:""}
          
       }
     }
     handleChange=(e)=>{//synthetic element
         console.log(e.target.value)//to read element
         var inputname =e.target.name
-        var newteddy ={...this.state.teddy}//creating an copy of object
-        newteddy[inputname]=e.target.value
-        this.setState({teddy:newteddy})
+        var newuser ={...this.state.user}//creating an copy of object
+        newuser[inputname]=e.target.value
+        this.setState({user:newuser})
 
 
      }
      handleSubmit=()=>{
-        console.log(this.state.teddy)
+        console.log(this.state.user)
     }
   render() {
     return (
@@ -27,17 +27,17 @@ export default class SampleFourClass extends Component {
         <form>
            
            <label htmlFor="username">Username :</label>
-           <input type="text" name="username"value={this.state.teddy.username} onChange={(e)=>{this.handleChange(e)}}/> <br/>
+           <input type="text" name="username"value={this.state.user.username} onChange={(e)=>{this.handleChange(e)}}/> <br/>
            <label htmlFor="password">Password :</label>
-           <input type="text" name="password"value={this.state.teddy.password} onChange={(e)=>{this.handleChange(e)}}/> <br/>
+           <input type="text" name="password"value={this.state.user.password} onChange={(e)=>{this.handleChange(e)}}/> <br/>
            <label htmlFor="emailaddress">Email Address :</label>
-           <input type="text" name="email"value={this.state.teddy.emailaddress} onChange={(e)=>{this.handleChange(e)}}/> <br/>
+           <input type="text" name="email"value={this.state.user.emailaddress} onChange={(e)=>{this.handleChange(e)}}/> <br/>
            <label htmlFor="dateofbirth">Date of Birth:</label> 
-            <input type="text" name="dateofbirth" value={this.state.teddy.dateofbirth} onChange={(e)=>{this.handleChange(e)}} /><br/>
+            <input type="text" name="dateofbirth" value={this.state.user.dateofbirth} onChange={(e)=>{this.handleChange(e)}} /><br/>
             <label htmlFor="height">Height :</label> 
-            <input type="text" name="height" value={this.state.teddy.height} onChange={(e)=>{this.handleChange(e)}} /><br/>
+            <input type="text" name="height" value={this.state.user.height} onChange={(e)=>{this.handleChange(e)}} /><br/>
             <label htmlFor="weight">Weight:</label> 
-            <input type="text" name="weight" value={this.state.teddy.weight} onChange={(e)=>{this.handleChange(e)}} /><br/>
+            <input type="text" name="weight" value={this.state.user.weight} onChange={(e)=>{this.handleChange(e)}} /><br/>
            
            <button onClick={this.handleSubmit} type="button">SUBMIT</button>
 
