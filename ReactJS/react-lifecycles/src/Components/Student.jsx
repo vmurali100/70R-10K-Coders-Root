994ba7afb9c933562b7d15ef6f068f0ee6e49d00
keerthiprasanna7 <keerthiprasannajntuh@gmail.com>
2022-09-08 18:28:15 +0530

    pushing lifecycles of reactjs

---
+++ ReactJS/react-lifecycles/src/Components/Student.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import ChildComp from './ChildComp'
+
+export default class Student extends Component {
+    constructor(props) {//component generate phase:egg stage/under construction:we don't write anything related to server or api in constructor as it is in generating phase
+      super(props)
+    
+      this.state = {
+        message:"Welcome to ReactJS life Cycle Hooks from Student Component-Parent",
+        isStudent:true
+         
+      }
+      console.log("Constructor lifecycle trigerred-Parent")
+    }
+  render() {//component render phase:it return html:no api related
+    console.log("render lifecycle trigerred-Parent")
+
+    return (
+      <div>
+        <button onClick={()=>{this.setState({isStudent:false})}}>Remove Student</button>
+        <button type="text"onClick={()=>{this.setState({message:"Changed in Parent"})}}>Change Message</button>
+        {this.state.isStudent && <ChildComp msg={this.state.message}/>}
+       
+      </div>
+    )
+  }
+  componentDidMount(){//component is ready:we can write api
+  console.log("Componentdidmount lifecycle trigerred-Parent")
+  }
+
+
+
+  //Component crosses 3  phases:
+//   1. Component Mounting Phase.//every component during rendering it undergoes 3 lifecycles.
+// a.constructor,b.render,c.componentdidmount[mounting lifecycles]
+
+//   2. Component Updating Phase.
+//   3. Component unmounting Phase.
+
+
+}
