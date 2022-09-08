f74f8c5911e4113746fb61dc08eed5e26d906833
Meetmsmanasa <106164924+Meetmsmanasa@users.noreply.github.com>
2022-09-08 01:24:15 -0700

    pushing CRUD SERVER API app5

---
+++ ReactJS/CRUD-SERVER-API/app5/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Mark } from './Components/Mark';
+
+function App() {
+  return (
+    <div className="App">
+      <Mark/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/CRUD-SERVER-API/app5/src/Components/Mark.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Mark = () => {
+  return (
+    <div>Mark</div>
+  )
+}
