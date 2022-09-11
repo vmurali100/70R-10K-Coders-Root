3cfa7c4d3ff33cf40070343e264a66fcf16d63ff
keerthiprasanna7 <keerthiprasannajntuh@gmail.com>
2022-09-11 21:17:57 +0530

    pushing project1

---
+++ ReactJS/Project1/app1/src/App.jsx
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './Components/Header';
+// import { Slider } from './Components/Slider';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Students } from './Components/Students';
+import { Teachers } from './Components/Teachers';
+import { Subjects } from './Components/Subjects';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/> 
+
+     {/* <Slider/> */}
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element ={ <Students/>}/>
+      <Route path='/subjects' element ={ <Subjects/>}/>
+      <Route path='/teachers' element ={ <Teachers/>}/>
+
+
+     </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/Project1/app1/src/Components/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export const Header = () => {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Carousel
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarCollapse"
+              aria-controls="navbarCollapse"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <li className="nav-item">
+                  {/* <a className="nav-link " aria-current="page" href="#">
+                    Students
+                  </a> */}
+                  <Link to="/">Students</Link> 
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link " aria-current="page" href="#">
+                    Subjects
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link " aria-current="page" href="#">
+                    Teachers
+                  </a>
+                </li>
+                
+              </ul>
+              
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
