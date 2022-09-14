539680108203b251c92fa9628b02a20ec15b1746
Chokkallasrikanth <srikanthchokkalla@gmail.com>
2022-09-14 21:33:35 +0530

    Edit Students are added in React-Routing

--- ReactJS/react-routing/src/Components/CreateStudents.jsx
+++ ReactJS/react-routing/src/Components/CreateStudents.jsx
@@ -1,42 +1,85 @@
-import axios from 'axios'
-import React,{useState} from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 export const CreateStudents = () => {
-    const [students] = useState({
-        id:"11",
-        name:"Chokkalla Srikanth",
-        username:"srikanthchokkalla",
-        email:"srikanthchokkalla@gmail.com"
-    })
-    const navigate = useNavigate()
-    const submittStudents=()=>{
-        axios.post("http://localhost:3000/Students",students).then(()=>{
-            navigate("/")
-        })
-    }
+  const [students, setstudents] = useState({
+    id: "",
+    name: "",
+    username: "",
+    email: "",
+  });
+  const { id, name, username, email } = students;
+  const navigate = useNavigate();
+  const submitStudents = () => {
+    axios.post("http://localhost:3000/Students/", students).then(() => {
+      navigate("/");
+    });
+  };
+  const handleChange = (e) => {
+    let newStudents = { ...students };
+    newStudents[e.target.name] = e.target.value;
+    setstudents(newStudents)
+  };
   return (
-    <div    className='container'>
-        <form>
-  <div className="mb-3">
-    <label  className="form-label">ID</label>
-    <input type="id" className="form-control" id="id" aria-describedby="id" />
-  </div>
-  <div className="mb-3">
-    <label  className="form-label">NAME</label>
-    <input type="name" className="form-control" id="name" />
-  </div>
-  <div className="mb-3">
-    <label  className="form-label">USERNAME</label>
-    <input type="username" className="form-control" id="username" />
-  </div>
-  <div className="mb-3">
-    <label  className="form-label">EMAIL</label>
-    <input type="email" className="form-control" id="email" />
-  </div>
-  <button type="button" className="btn btn-primary" onClick={submittStudents}>Submit</button>
-</form>
+    <div className="container">
+      <form>
+        <div className="mb-3">
+          <label className="form-label">ID</label>
+          <input
+            type="text"
+            className="form-control"
+            name="id"
+            value={id}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">NAME</label>
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            value={name}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">USERNAME</label>
+          <input
+            type="text"
+            className="form-control"
+            name="username"
+            value={username}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">EMAIL</label>
+          <input
+            type="text"
+            className="form-control"
+            name="email"
+            value={email}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={submitStudents}
+        >
+          Submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
--- ReactJS/react-routing/src/Components/EditStudents.jsx
+++ ReactJS/react-routing/src/Components/EditStudents.jsx
@@ -1,7 +1,93 @@
-import React from 'react'
+/* eslint-disable eqeqeq */
+import axios from "axios";
+import React, { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 export const EditStudents = () => {
+  const urlObj = useParams();
+  const navigate = useNavigate();
+  let url = "http://localhost:3000/Students/";
+
+  const student = JSON.parse(localStorage.getItem("Students"))
+  const response = student.find((std)=>std.id == urlObj.id)
+
+  const [students, setstudents] = useState(response);
+  const { id, name, username, email } = students;
+
+
+
+  const handleChange = (e) => {
+    let newStudents = { ...students };
+    newStudents[e.target.name] = e.target.value;
+    setstudents(newStudents);
+  };
+
+  const updateStudents = () => {
+   axios.put(url+urlObj.id,students).then(()=>{
+    navigate("/")
+   })
+  };
+ 
   return (
-    <div>EditStudents</div>
-  )
-}
+    <div className="container">
+      <h2>Edit Students</h2>
+      <form>
+        <div className="mb-3">
+          <label className="form-label">ID</label>
+          <input
+            type="text"
+            className="form-control"
+            name="id"
+            value={id}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">NAME</label>
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            value={name}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">USERNAME</label>
+          <input
+            type="text"
+            className="form-control"
+            name="username"
+            value={username}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">EMAIL</label>
+          <input
+            type="text"
+            className="form-control"
+            name="email"
+            value={email}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={updateStudents}
+        >
+          Update Students
+        </button>
+      </form>
+    </div>
+  );
+};
