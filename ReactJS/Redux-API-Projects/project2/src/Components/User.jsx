23a23311fabb9722adcf22714c0e8e359ac2169c
Chokkallasrikanth <srikanthchokkalla@gmail.com>
2022-09-19 12:05:07 +0530

    changes

--- ReactJS/Redux-API-Projects/project2/src/Components/User.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useSelector } from "react-redux";
-
-export const User = () => {
-  const userDetails = useSelector((state) => state.user);
-  console.log(userDetails.user);
-  return <div>User</div>;
-};
