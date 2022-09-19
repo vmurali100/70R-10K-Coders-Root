13e194f2e6898267626001592b137b957c7dfded
Meetmsmanasa <106164924+Meetmsmanasa@users.noreply.github.com>
2022-09-19 07:47:27 -0700

    pushing react redux API project1

---
+++ ReactJS/REDUX-API-PROJECTS/redux-project1/src/App.js
@@ -0,0 +1,14 @@
+import { useSelector } from 'react-redux';
+import './App.css';
+import { Users } from './Components/Users';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <Users/>
+    </div>
+  );
+}
+
+export default App;
