6fc53df9066e5f336b3132a5ec698f9ef60dc1a6
876sunny <sunnydsp876@gmail.com>
2022-09-23 01:35:02 +0530

    add

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Header } from "./Components/Header";
+import { Slider } from "./Components/Slider";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { Students } from "./Components/Students";
+import { Subjects } from "./Components/Subjects";
+import { Teachers } from "./Components/Teachers";
+import { TeachersDetails } from "./Components/TeachersDetails";
+import { StudentDetails } from "./Components/StudentDetails";
+import { CreateStudent } from "./Components/CreateStudent";
+import { EditStudent } from "./Components/EditStudent";
+import { DeleteStudent } from "./Components/DeleteStudent";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* <Slider/> */}
+      <BrowserRouter>
+      <Header />
+
+        <Routes>
+        <Route path="/" element={<Students/>} />
+        <Route path="/createStudent" element={<CreateStudent/>} />
+        <Route path="/editStudent/:id" element={<EditStudent/>} />
+        <Route path="/deleteStudent/:id" element={<DeleteStudent/>} />
+        <Route path="/:id" element={<StudentDetails/>} />
+        <Route path="/subjects" element={<Subjects/>} />
+        <Route path="/teachers" element={<Teachers/>} />
+        <Route path="/teachers/:id" element={<TeachersDetails/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
