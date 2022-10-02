5720eeebcb7fee534ef457c8f02cbd447cc878b3
SeelamMeena <bmeenu05@gmail.com>
2022-10-02 17:51:49 +0530

    useMemo

---
+++ ReactJS/ReactHooks/usememo/src/Components/Counter.js
@@ -0,0 +1,39 @@
+import React,{useMemo, useState} from 'react'
+
+const Counter = () => {
+    const [counterOne, setcounterOne] = useState(0);
+    const [counterTwo, setcounterTwo] = useState(0);
+
+    const increOne=()=>{
+        setcounterOne(counterOne+1)
+    }
+
+    const increTwo=()=>{
+        setcounterTwo(counterTwo+1)
+    }
+
+    //when you want to potimize the the cacahe of a function we use useMemo
+
+    const isEven=useMemo(()=>{
+        let i=0;
+        while(i<2000000000) i++
+        return counterOne % 2 == 0;
+    },[counterOne])
+
+    
+  return (
+    <div>
+
+    <div>
+        <button type='button' onClick={increOne}>Count One- {counterOne}</button>
+        <span>{isEven ? 'Even' : 'Odd'}</span>
+    </div>
+    <div>
+        <button type='button' onClick={increTwo}>Count Two- {counterTwo}</button>
+    </div>
+
+    </div>
+  )
+}
+
+export default Counter
